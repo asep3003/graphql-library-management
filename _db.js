d8d6ed24dad0fe3d4d57355f929af7e13a749883
asep3003 <saepulloha133@gmail.com>
2025-06-13 17:19:24 +0700

    feat: basic schema & query

---
+++ _db.js
@@ -0,0 +1,35 @@
+const books = [
+  {
+    id: "1",
+    title: "Atomic Habits",
+    author: "James Clear",
+    published_at: "2018-10-16",
+    category: "Self-Help",
+    total: 10,
+  },
+  {
+    id: "2",
+    title: "The Subtle Art of Not Giving a F*ck",
+    author: "Mark Manson",
+    published_at: "2016-09-13",
+    category: "Self-Help",
+    total: 10,
+  },
+];
+
+const members = [
+  {
+    id: "1",
+    name: "Mohamad Asep Saepulloh",
+    email: "saepulloha133@gmail.com",
+    verified: true,
+  },
+  {
+    id: "2",
+    name: "Mochamad Adiel Bawana",
+    email: "adielbwn9@gmail.com",
+    verified: false,
+  },
+];
+
+export { books, members };
