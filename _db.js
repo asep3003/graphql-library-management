5892ebb5a022e7c3b5cd860f45c140c0fb44c618
asep3003 <saepulloha133@gmail.com>
2025-06-13 22:25:43 +0700

    feat: related data

--- _db.js
+++ _db.js
@@ -1,6 +1,6 @@
 const books = [
   {
-    id: "1",
+    id: "book-1",
     title: "Atomic Habits",
     author: "James Clear",
     published_at: "2018-10-16",
@@ -8,7 +8,7 @@ const books = [
     total: 10,
   },
   {
-    id: "2",
+    id: "book-2",
     title: "The Subtle Art of Not Giving a F*ck",
     author: "Mark Manson",
     published_at: "2016-09-13",
@@ -16,7 +16,7 @@ const books = [
     total: 10,
   },
   {
-    id: "3",
+    id: "book-3",
     title: "The Power of Now",
     author: "Eckhart Tolle",
     published_at: "2006-10-01",
@@ -24,7 +24,7 @@ const books = [
     total: 10,
   },
   {
-    id: "4",
+    id: "book-4",
     title: "The Power of Habit",
     author: "Charles Duhigg",
     published_at: "2009-10-01",
@@ -35,29 +35,46 @@ const books = [
 
 const members = [
   {
-    id: "1",
+    id: "member-1",
     name: "Mohamad Asep Saepulloh",
     email: "saepulloha133@gmail.com",
     verified: true,
   },
   {
-    id: "2",
+    id: "member-2",
     name: "Mochamad Adiel Bawana",
     email: "adielbwn912@gmail.com",
     verified: false,
   },
   {
-    id: "3",
+    id: "member-3",
     name: "Mochamad Aditya",
     email: "aditya133@gmail.com",
     verified: true,
   },
   {
-    id: "4",
+    id: "member-4",
     name: "Anggana Putra",
     email: "angganaputra9@gmail.com",
     verified: true,
   },
 ];
 
-export { books, members };
+const lendings = [
+  {
+    id: "lend-1",
+    book_id: "book-1",
+    member_id: "member-1",
+    borrowed_at: "2024-06-13",
+    returned_at: "2025-06-13",
+  },
+  {
+    id: "lend-2",
+    book_id: "book-2",
+    member_id: "member-1",
+    borrowed_at: "2024-06-13",
+    returned_at: "2025-06-13",
+  },
+];
+
+export { books, members, lendings };
